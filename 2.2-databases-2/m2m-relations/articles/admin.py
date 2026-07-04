a58404100d5131cf2b3d652830c4ed30d451fae7
Roman Zolotarev <roman@zen4apps.ru>
2026-07-04 19:44:29 +0300

    Реализация ДЗ 3.2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        if any(self.errors):
+            return
+        flags_count = 0
+
+        for form in self.forms:
+            if self.can_delete and self._should_delete_form(form):
+                continue
+
+            if form.cleaned_data.get("is_main", False):
+                flags_count += 1
+
+            if flags_count > 1:
+                raise ValidationError('Не более одной главной категории')
+
+        super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
